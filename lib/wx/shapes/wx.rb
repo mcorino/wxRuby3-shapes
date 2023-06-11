3ca2d0db0d784702eafd95620393a7a783977589
Martin Corino <mcorino@m2c-software.nl>
2023-06-11 12:09:23 +0200

    improve; add docu

--- lib/wx/shapes/wx.rb
+++ lib/wx/shapes/wx.rb
@@ -5,11 +5,21 @@ module Wx
 
   class Wx::RealPoint
 
+    # Returns distance from this point to given point.
+    # @param [Wx::RealPoint,Wx::Point,Array(Integer,Integer)] pt2
+    # @return [Float] distance to given point
     def distance_to(pt2)
-      Math.sqrt((pt2.x - self.x)*(pt2.x - self.x) + (pt2.y - self.y)*(pt2.y - self.y))
+      if Array === arg && arg.size == 2
+        to_x, to_y = arg
+      else
+        to_x = pt2.x; to_y = pt2.y
+      end
+      Math.sqrt((to_x - self.x)*(to_x - self.x) + (to_y - self.y)*(to_y - self.y))
     end
     alias :distance :distance_to
 
+    # Returns this point as a Wx::Size
+    # @return [Wx::Size]
     def to_size
       Wx::Size.new(self.x.to_i, self.y.to_i)
     end
